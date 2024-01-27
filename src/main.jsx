5f3ed76626a3aea23034f745772d2c549fc56490
HarshForreal <hp673315@gmail.com>
2024-01-27 22:37:04 +0530

    Changes for vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,10 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-
+import App from "./App.jsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Layout from "./Layout.jsx";
 import Resume from "./Pages/Resume.jsx";
 import About from "./Pages/About.jsx";
